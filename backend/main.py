60457aa7a08e7a1c3f5f1cf12effd67ad879d7f4
Yuri Gladkov <cyberglad@mail.ru>
2025-03-05 12:14:49 +0100

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -2,12 +2,23 @@ import os
 from fastapi import FastAPI, UploadFile, File
 import whisper
 from dotenv import load_dotenv
+from fastapi.middleware.cors import CORSMiddleware
+
 
 # Load API key from .env file
 load_dotenv()
 model = whisper.load_model("base")  # Use "tiny", "small", "medium", "large" if needed
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (for local testing)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.post("/upload")
 async def upload_audio(file: UploadFile = File(...)):
