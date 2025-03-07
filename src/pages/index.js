41f3b90ba5fafbb5eca2d331ffafebec69d3255b
Yuri Gladkov <cyberglad@mail.ru>
2025-03-07 17:07:55 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ export default function Home() {
     formData.append("file", file);
 
     try {
-      const response = await fetch("http://localhost:8000/upload", {
+          const response = await fetch("https://audiotranscriber-skor.onrender.com/upload", {
         method: "POST",
         body: formData,
       });
