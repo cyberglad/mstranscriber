83f05dc91ca2445f9797fa603328e6851abdd9d8
Yuri Gladkov <cyberglad@mail.ru>
2025-03-10 14:36:04 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",  // Enables static export for deployment
-  distDir: "out",    // Ensures output directory is 'out'
-};
-
-module.exports = nextConfig;
